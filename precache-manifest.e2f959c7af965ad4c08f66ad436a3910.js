133f46ad6c22e74e172eaaf49e6e2a542a749ada
Mohd Umar Alam <umaralam48@gmail.com>
2019-09-26 22:22:43 +0530

    Updates

--- precache-manifest.e2f959c7af965ad4c08f66ad436a3910.js
+++ precache-manifest.e2f959c7af965ad4c08f66ad436a3910.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71a87693c87f2309c682d0ffb4c57591",
+    "revision": "5dab79049fb97c0ea2ba1597cf7634c5",
     "url": "/socialape-client/index.html"
   },
   {
-    "revision": "93f21f86f713d89cd362",
+    "revision": "7efe55f027323ba586f8",
     "url": "/socialape-client/static/css/main.ac8c1c64.chunk.css"
   },
   {
-    "revision": "becd93d1d7dbe723b4b1",
-    "url": "/socialape-client/static/js/2.f367d3ea.chunk.js"
+    "revision": "e8c215af0da508e16939",
+    "url": "/socialape-client/static/js/2.df7d2630.chunk.js"
   },
   {
-    "revision": "93f21f86f713d89cd362",
-    "url": "/socialape-client/static/js/main.b54e3d14.chunk.js"
+    "revision": "7efe55f027323ba586f8",
+    "url": "/socialape-client/static/js/main.0d83dba4.chunk.js"
   },
   {
     "revision": "0555cd65fe2d741a59c9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/socialape-client/precache-manifest.0d6962fdc97b33684ecb6f38f2476eea.js"
+  "/socialape-client/precache-manifest.e2f959c7af965ad4c08f66ad436a3910.js"
 );
 
 self.addEventListener('message', (event) => {
